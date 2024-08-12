9f49e0ec39c9d13cd64458512255808085591e61
JJM3485 <geumman45@gmail.com>
2024-08-12 20:22:19 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import $ from 'jquery'
+
 //베이스 사운드 함수
 export function playSound(name,vol,loop = false) {
   const audio = new Audio('./'+name);
@@ -6,4 +8,15 @@ export function playSound(name,vol,loop = false) {
   audio.play();
   return audio;
 }
-playSound("Destroyed World.mp3",1);
+playSound("backsound.mp3",0.5);
+
+
+function on(){
+  const onsound = new Audio('./clicksound.mp3');
+  onsound.play();
+  alert("하지마루요"); 
+}
+
+
+$('.btn').on('click', function() {on()});
+
