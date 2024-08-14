f1690e355521affea39a3c7d29c4da632ef2a95d
JJM3485 <geumman45@gmail.com>
2024-08-14 21:08:42 +0900

    Add files via upload

---
+++ irohatext.js
@@ -0,0 +1,18 @@
+export const IROHATEXT1 = [
+    {"TEXT1":"늦은 밤", "select":false},
+
+    {"TEXT1":"대화문1","select":false},
+
+    {"TEXT1":"대화문2","select":false},
+
+    {"TEXT1":"대화문3","select":true}, //선택지 발생
+
+    {"TEXT1":"대화문4","select":false},
+
+    {"TEXT1":"대화문5","select":false},
+]
+
+
+export const SELECTTEX = [
+    {"select1":"선택지1", "select2":"선택지2"},
+]
--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 import $ from 'jquery'
+import {IROHATEXT1,SELECTTEX} from "./irohatext"
 
+//선택지
+let choicecheck;      //선택지 골랐는지 확인
+let choiceevent;      //어떤 선택지사항인지
+let choicenow=0;      //어떤 선택지고름?
+let choicesuccess           //선택지 성공여부
 
+let Conversationtext  // 현재 나온 타이핑
+let currentCharIndex  // 현재 타이핑 위치
+let typingSpeed = 60; // 타이핑 속도 (밀리초)
+let skiptext=false
 
+let i = 0; //대화
+let j = 0; //선택지
+let point = 100; //호감도
 
+const $conversation = $(".conversation");
+
+function typeCharacter() {
+  if (currentCharIndex < Conversationtext.length) {
+    $conversation.text($conversation.text() + Conversationtext.charAt(currentCharIndex));
+    currentCharIndex++;
+    setTimeout(typeCharacter, typingSpeed);
+  } 
+  else {
+    $conversation.text(Conversationtext); // 타이핑이 끝나면 전체 텍스트 표시
+  }
+}
+
+//텍스트 출력 초기화시키는곳
+function PrintText(text){
+  $conversation.text('');
+  currentCharIndex=0;
+  Conversationtext=text
+  typeCharacter();
+}
 
 //베이스 사운드 함수
 export function playSound(name,vol,loop = false) {
@@ -12,7 +45,7 @@ export function playSound(name,vol,loop = false) {
   audio.play();
   return audio;
 }
-//playSound("backsound.mp3",0.5);
+const PS = playSound("backsound.mp3",0.5);
 
 
 function on(){
@@ -20,12 +53,41 @@ function on(){
   onsound.play();
   $('.gamebox').show();
   $('.middle').hide();
+  PS.pause();
+  const PS2 = playSound("inclass.mp3",0.5);
+  PrintText(IROHATEXT1[i]["TEXT1"]);
 }
 
 function likepoint(){
-  var point = 100;
   $('.like').text(point);
 }
+
+$(".gamebox").on('click', function() {
+  i++;
+  PrintText(IROHATEXT1[i]["TEXT1"]);
+  if (IROHATEXT1[i]["select"] == true){
+    $('.wall').show();
+    $('.selectbox1').show();
+    $('.selectbox1').text(SELECTTEX[j]["select1"]);
+    $('.selectbox2').show();
+    $('.selectbox2').text(SELECTTEX[j]["select2"]);
+    j++;
+    $('.selectbox1').on('click', function() {
+      point = point + 10;
+      likepoint();
+      $('.selectbox1').hide();
+      $('.selectbox2').hide();
+      $('.wall').hide();
+    });
+    $('.selectbox2').on('click', function() {
+      point = point - 10;
+      likepoint();
+      $('.selectbox1').hide();
+      $('.selectbox2').hide();
+      $('.wall').hide();
+    });
+  }
+});
 
 
 
@@ -36,3 +98,9 @@ $('.btn').on('click', function() {on()});
 $('.ch').attr('src','./ch1.png');
 
 likepoint();
+
+$('.selectbox1').hide();
+
+$('.selectbox2').hide();
+
+$('.wall').hide();
